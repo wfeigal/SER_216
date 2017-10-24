41804e1a304f7b42bea279db729976f317163546
Bill Feigal <Bill Feigal@HomeDesktop>
2017-10-24 18:50:43 -0500

    setup for Junit testing

---
+++ src/Core/BankAccount.java
@@ -0,0 +1,114 @@
+package Core;
+
+public class BankAccount {
+
+	private String accountNumber = "";
+	private String accountHolder = "";
+	private double balance = 0;
+	private int accountType = 0;
+	
+	/**
+	 * Default constructor
+	 */
+	BankAccount(){
+		this.balance = 0;
+		this.accountType = 0;
+		this.accountNumber = "none";
+		this.accountHolder = "Unknown";
+	}
+	
+	/**
+	 * Parameterized constructor
+	 * 
+	 * @param bal Initial balance
+	 * @param type Account type 
+	 * @param num Account number
+	 * @param name Name of account holder
+	 */
+	BankAccount(double bal, int type, String num, String name){
+		this.balance = bal;
+		this.accountType = type;
+		this.accountNumber = num;
+		this.accountHolder = name;
+	}
+	/**
+	 * 
+	 * @return Account number
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	/**
+	 * 
+	 * @param accountNumber
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	/**
+	 * 
+	 * @return Account holder name
+	 */
+	public String getAccountHolder() {
+		return accountHolder;
+	}
+	/**
+	 * 
+	 * @param accountHolder
+	 */
+	public void setAccountHolder(String accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+	/**
+	 * 
+	 * @return Current balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	/**
+	 * 
+	 * @param balance
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	/**
+	 * 
+	 * @return Account type
+	 */
+	public int getAccountType() {
+		return accountType;
+	}
+	/**
+	 * 
+	 * @param accountType
+	 */
+	public void setAccountType(int accountType) {
+		this.accountType = accountType;
+	}
+	
+	public double calculateTotalBalance() {
+		double rate = this.getInterestRate();
+		if (rate == 0 )
+			return 0;
+		return this.balance + (balance * rate/100);
+	}
+	
+	public double getInterestRate() {
+		double intRate = 0;
+		
+		switch (accountType){
+			case 1: intRate = 0.5;
+				break;
+			case 2: intRate = 4.5;
+				break;
+			case 3: intRate = 1.0;
+				break;
+			case 4: intRate = 15;
+				break;
+			default: return 0;
+		}
+		return intRate;
+	}
+}
